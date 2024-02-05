e36c533a998b8191ad214f128704a9e1e7e2a9fa
Mohammed Armaan <mr_mohammed_armaan@icloud.com>
2024-02-05 15:11:34 +0000

    added pokemon home page

--- src/server.js
+++ src/server.js
@@ -2,7 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.use("/example", express.static("example"))
+
+app.use("/Pokemon", express.static("pokemon"));
+
+app.use("/example", express.static("example"));
+
+
   
 app.listen(5001, () => {
     console.log("Server is listening on port 5001");
